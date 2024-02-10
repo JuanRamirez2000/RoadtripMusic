2617ef0cdcaaa61a0344a24c94edc3574b06d433
Juan Ramirez <juanpram2000@gmail.com>
2024-02-09 23:09:25 -0800

    chore: Resolved previous dependencies from google maps and from react query

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,9 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-await import("./env.mjs");
-
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
   experimental: {
-    appDir: true,
     typedRoutes: true,
-    serverActions: true
-  }
+    serverActions: true,
+  },
 };
 export default config;
