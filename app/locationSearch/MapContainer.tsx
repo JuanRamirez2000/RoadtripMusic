97d19658fa44fd324af7324718ecfa9ed41ade2a
Juan Ramirez <juanpram2000@gmail.com>
2024-02-11 16:05:27 -0800

    feat: dark mode and light mode for the map has been added

--- app/locationSearch/MapContainer.tsx
+++ app/locationSearch/MapContainer.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { useTheme } from "next-themes";
 import { Map } from "react-map-gl";
 
 const MAPBOX_ACCESS_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
 
 export default function MapContainer() {
+  const { theme } = useTheme();
   return (
     <Map
       mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
@@ -13,8 +15,12 @@ export default function MapContainer() {
         latitude: 37.8,
         zoom: 14,
       }}
-      style={{ width: 600, height: 400 }}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
+      style={{ width: "100%", height: "100%" }}
+      mapStyle={
+        theme === "light"
+          ? "mapbox://styles/mapbox/streets-v9"
+          : "mapbox://styles/mapbox/dark-v11"
+      }
     />
   );
 }
--- app/locationSearch/page.tsx
+++ app/locationSearch/page.tsx
@@ -2,7 +2,8 @@ import MapContainer from "./MapContainer";
 
 export default function LocationsSearchPage() {
   return (
-    <div className="h-screen w-screen">
+    <div className="flex h-screen w-screen flex-row">
+      <div className="h-full w-1/2"></div>
       <div className="h-full w-1/2">
         <MapContainer />
       </div>
